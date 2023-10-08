10687075d94d0748443deb25a3861522e0f1b544
Maxence Lequeux <maxenceleq@gmail.com>
2023-10-08 22:07:01 +0200

    Make server struct, env example file, env utils

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "github.com/joho/godotenv"
+import (
+	"log"
+	"net/http"
+
+	"github.com/GuGules/cookifyr/utils"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/joho/godotenv"
+)
+
+const defaultAddr = ":3000"
+
+type Server struct {
+	Addr   string
+	Router *chi.Mux
+}
+
+func NewServer(addr string) *Server {
+	return &Server{
+		Addr:   addr,
+		Router: chi.NewRouter(),
+	}
+}
+
+func (s *Server) Serve() error {
+	log.Printf("server will listen on %s", s.Addr)
+	return http.ListenAndServe(s.Addr, s.Router)
+}
 
 func main() {
 	godotenv.Load()
+
+	addr := utils.GetEnvOr("HTTP_ADDRESS", defaultAddr)
+
+	s := NewServer(addr)
+	s.Router.Use(middleware.Logger)
+	s.Router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome"))
+	})
+	s.Serve()
 }
